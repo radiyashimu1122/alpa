10e721a7d34728eda14e6183bc13382f512eb5d6
radiyashimu1122 <radiyashimu@gmail.com>
2024-02-16 12:06:23 -0800

    some update

---
+++ script/alpa.js
@@ -0,0 +1,11 @@
+// function play(params) {
+//  // console.log('play start now');
+//  const homeSection = document.getElementById('home-screen');
+// // console.log(homeSection.classList);
+// homeSection.classList.add('hidden');
+// const playgroundSection = document.getElementById('playground');
+// playgroundSection.classList.remove('hidden');
+// }
+function play(params) {
+ hideElementById('home-screen')
+}
---
+++ utility.js
@@ -0,0 +1,8 @@
+function hideElementById(elementId) {
+ const element = document.getElementById(elementId);
+ element.classList.add('hidden');
+}
+function                                                                                                                                   ElementById(elementId) {
+ const element = document.getElementById(elementId);
+ element.classList.add('hidden');
+}
